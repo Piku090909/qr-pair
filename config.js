f80dd9c837e29617320df55c7fa89cdc548e5974
alex piku <122716205+Piku090909@users.noreply.github.com>
2024-07-04 14:39:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://pikubalur:<HqKszghJSAP93Lzg>@cluster0.banbauq.mongodb.net/",
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://soonc814:KYMIf1d5eLT8EWip@cluster0.bwczws5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   SESSION_NAME: process.env.SESSION_NAME || "À𝖑è𝖝 𝕻ì𝖐ù"
 };
